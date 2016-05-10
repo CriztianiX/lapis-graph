7012f26db7f7890f66340b606fd183ce867b2363
Cristian Haunsen <cristianhaunsen@gmail.com>
2016-05-10 04:06:56 +0000

    update test port

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl -XPOST 127.0.0.1:8080 -H "Content-Type: application/json" -d '
+curl -XPOST 127.0.0.1:9900 -H "Content-Type: application/json" -d '
 {
 	"arguments" : {
 	 "id" : 2
