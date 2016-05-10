c88296205e1369d4ae067e68df0e696403fcb0cd
Cristian Haunsen <cristianhaunsen@gmail.com>
2016-05-10 04:05:42 +0000

    Change port and instructions for db

--- config.lua
+++ config.lua
@@ -1,5 +1,6 @@
 local config = require("lapis.config")
 config({"development", "production"}, {
+  port = 9900,
   postgres = {
     host     = "127.0.0.1",
     database = "graph0",
--- install.sh
+++ install.sh
@@ -1,2 +1,7 @@
+#!/bin/bash
+
+echo "CREATE USER graph0 WITH PASSWORD 'qwe123';"
+echo "CREATE DATABASE graph0 OWNER graph0;"
+
 cd deps ; git clone https://github.com/bjornbytes/graphql-lua
 cd graphql-lua ; sudo luarocks make
